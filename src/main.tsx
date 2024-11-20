4f01cb6fac3c871a6fdf8157df76027304a6571b
HyunTaek Oh <oht222@naver.com>
2024-11-20 15:21:20 +0900

    setup: tailwindcss 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routesConfig = [
+  {
+    path: '',
+    element: '<div>Home</div>',
+  },
+];
+
+const router = createBrowserRouter(
+  routesConfig.map(route => ({
+    path: route.path,
+    element: route.element,
+  }))
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
