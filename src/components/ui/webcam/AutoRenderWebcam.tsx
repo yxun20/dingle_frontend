952fdab6688febd5a12a1962627588881f0c8409
HyunTaek Oh <oht222@naver.com>
2024-11-26 17:21:29 +0900

    feat: 페이지 진입시 웹캠 바로 랜더되도록 수정

---
+++ src/components/ui/webcam/AutoRenderWebcam.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import babyMonitoringImage from '@/assets/babyImage3.png';
+
+const AutoRenderWebcam = () => {
+  const [isWebcamOn, setIsWebcamOn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const webcamRef = useRef(null);
+
+  useEffect(() => {
+    const checkWebcamStatus = async () => {
+      setLoading(true);
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const hasCamera = devices.some(device => device.kind === 'videoinput');
+        if (hasCamera) {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          if (stream) {
+            setIsWebcamOn(true);
+            stream.getTracks().forEach(track => track.stop());
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkWebcamStatus();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div>
+          <p>웹캠 상태를 확인 중...</p>
+          <img
+            src={babyMonitoringImage}
+            alt="Baby Monitoring"
+            className="w-full max-w-xs h-auto border-4 border-red-500"
+          />
+        </div>
+      ) : isWebcamOn ? (
+        <Webcam ref={webcamRef} audio={true} videoConstraints={{ width: 320, height: 400, facingMode: 'user' }} />
+      ) : (
+        <p>웹캠을 사용할 수 없습니다.</p>
+      )}
+    </div>
+  );
+};
+
+export default AutoRenderWebcam;
