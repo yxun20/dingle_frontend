7796966f5d91d09e809d385e4e029243b927fb90
HyunTaek Oh <oht222@naver.com>
2024-12-11 15:37:10 +0900

    feat: api 주소 프록시 처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import react from '@vitejs/plugin-react-swc'
+import tailwindcss from 'tailwindcss';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_BASE_URL = `${env.VITE_API_BASE_URL ?? 'http://localhost:8080'}`;
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
+});
