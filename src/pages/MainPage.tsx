2fdbeb80d3a6fe58d5121ab2e29f70cf7b605df9
성영준 <s.yxun20@gmail.com>
2024-12-14 17:14:53 +0900

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -63,7 +63,7 @@ function MainPage() {
         </div>
         {/* 하단 아이 보기 버튼 */}
         <button
-          className="w-full py-4 text-white bg-green-500 rounded-md hover:bg-[CDE4F2] focus:outline-none focus:bg-green-700"
+          className="w-full py-4 text-white bg-green-400 rounded-md hover:bg-green-500 focus:outline-none focus:bg-green-600"
           onClick={() => navigate('/dash')}
         >
           아이 보러가기
