9bd25a40e3772ba00c46c53c9194070ab5bca4c6
HyunTaek Oh <oht222@naver.com>
2024-12-19 17:13:11 +0900

    fix: UI 깨지는 이슈 수정

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileCard from '@/components/ui/profile';
 import babyProfile from '@/assets/babyprofile.png';
 import momProfile from '@/assets/momprofile.png';
@@ -8,8 +8,8 @@ import httpClient from '@/lib/client/http-client';
 
 const MyPage = () => {
   const [babyName, setBabyName] = useState('아기이름');
-  const [birthDate, setBirthDate] = useState('2000-01-01');  //서버에서 가져오눈 날짜 데이터 
-  const [babyAge, setBabyAge] = useState('생후 0일');  //계산한 애기 나이
+  const [birthDate, setBirthDate] = useState('2000-01-01'); //서버에서 가져오눈 날짜 데이터
+  const [babyAge, setBabyAge] = useState('생후 0일'); //계산한 애기 나이
 
   const [momPhone, setMomPhone] = useState('긴급연락처를 적어주세요');
   const [dadPhone, setDadPhone] = useState('긴급연락처를 적어주세요');
@@ -26,7 +26,6 @@ const MyPage = () => {
 
         const calculatedDays = calculateDaysFromBirth(baby.birthDate);
         setBabyAge(`생후 ${calculatedDays}일`);
-        
 
         setMomPhone(parentContacts?.momPhoneNumber || '긴급연락처를 적어주세요');
         setDadPhone(parentContacts?.dadPhoneNumber || '긴급연락처를 적어주세요');
@@ -36,9 +35,9 @@ const MyPage = () => {
     };
 
     fetchProfileData();
-  }, []); 
+  }, []);
 
-  //아기 나이 계산 함수 
+  //아기 나이 계산 함수
   const calculateDaysFromBirth = (birthDate: string) => {
     const birth = new Date(birthDate); // baby.birthDate를 Date 객체로 변환
     const today = new Date(); // 현재 날짜
@@ -57,16 +56,15 @@ const MyPage = () => {
         babyName: babyName,
         birthDate: birthDate,
       },
-      parentContacts:{
+      parentContacts: {
         momPhoneNumber: momPhone,
         dadPhoneNumber: dadPhone,
-      }
+      },
     };
     try {
       // POST 요청으로 데이터 전송
       const response = await httpClient.put('/users/me', requestBody);
       console.log('유저데이터 저장 성공:', response.data);
-
     } catch (error) {
       console.error('서버 연결 실패 :', error);
       alert('문제가 발생했습니다. 다시 시도해주세요.');
@@ -82,19 +80,13 @@ const MyPage = () => {
 
   return (
     <div className="w-[386px] h-[823px] mx-auto pt-2 pb-4 px-4 bg-[#E8F8F5] rounded-lg flex flex-col items-center">
-      
       {/* 뒤로가기 버튼 컴포넌트 to='이동할 페이지'*/}
       <BackButton to="/" className={'w-full flex justify-start pl-4 mt-4'} />
-      
-        {/* 프로필 카드 섹션 */}
+
+      {/* 프로필 카드 섹션 */}
       <div className="flex flex-col items-center justify-around p-20">
         {/* 아기 프로필 */}
-        <ProfileCard
-          name={babyName}
-          age={babyAge}
-          imgSrc={babyProfile}
-          alignment="left"
-        />
+        <ProfileCard name={babyName} age={babyAge} imgSrc={babyProfile} alignment="left" />
 
         {/* 엄마 프로필 */}
         <ProfileCard
@@ -114,21 +106,15 @@ const MyPage = () => {
           alignment="left"
         />
       </div>
-      
-    
+
       {/* 수정 버튼 */}
-      <button
-        onClick={handleEditClick}
-        className="text-sm text-blue-500 mt-auto hover:underline"
-      >
+      <button onClick={handleEditClick} className="text-sm text-blue-500 mt-auto hover:underline">
         수정하기
       </button>
 
       {/* 수정 모달 */}
       {isEditing && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-        >
+        <div className="w-[390px] h-[823px] fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-[300px]">
             <h2 className="text-lg font-bold mb-4">정보 수정</h2>
             <form>
@@ -140,7 +126,7 @@ const MyPage = () => {
                   type="text"
                   id="name"
                   value={babyName}
-                  onChange={(e) => setBabyName(e.target.value)}
+                  onChange={e => setBabyName(e.target.value)}
                   placeholder="우리 아이 이름"
                   className="w-full p-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
@@ -154,7 +140,7 @@ const MyPage = () => {
                   type="date"
                   id="birthDate"
                   value={birthDate}
-                  onChange={(e) => setBirthDate(e.target.value)}
+                  onChange={e => setBirthDate(e.target.value)}
                   className="w-full p-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
                 />
@@ -167,7 +153,7 @@ const MyPage = () => {
                   type="tel"
                   id="momPhone"
                   value={momPhone}
-                  onChange={(e) => setMomPhone(e.target.value)}
+                  onChange={e => setMomPhone(e.target.value)}
                   placeholder="010-"
                   className="w-full p-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
@@ -181,7 +167,7 @@ const MyPage = () => {
                   type="tel"
                   id="dadPhone"
                   value={dadPhone}
-                  onChange={(e) => setDadPhone(e.target.value)}
+                  onChange={e => setDadPhone(e.target.value)}
                   placeholder="010-"
                   className="w-full p-2 mt-1 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
@@ -195,10 +181,7 @@ const MyPage = () => {
               >
                 취소
               </button>
-              <button
-                onClick={handleSave}
-                className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
-              >
+              <button onClick={handleSave} className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600">
                 저장
               </button>
             </div>
